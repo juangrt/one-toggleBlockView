1e47ef36919207ac66de9080075550ed1aa4bf2b
Juan Garzon <jgarzon@grassroots-tech.com>
2016-03-02 17:58:54 -0500

    Mocked data to build mock blocks.

--- index.js
+++ index.js
@@ -4,8 +4,46 @@ import ReactDOM from 'react-dom';
 import ClusterToggleView from './src/ClusterToggleView';
 
 
+
+/*
+ * Accepted Model
+ * Number - Id (Should be unique)
+ * Array - Server List
+ * String - Environment Name
+ * String - Status
+ * String?- Version
+ */
+var mockDataThumb1 = {
+  id: 1,
+  serverList: ["192.168.1.1" , "192.168.1.2"] ,
+  name: "dev" ,
+  status: "Pending",
+  version: "1.5"
+};
+
+var mockDataThumb2 = {
+  id: 1,
+  serverList: ["192.168.1.1" , "192.168.1.2"] ,
+  name: "dev2" ,
+  status: "Pending",
+  version: "1.5"
+};
+
+var mockDataThumb3 = {
+  id: 1,
+  serverList: ["192.168.1.1" , "192.168.1.2"] ,
+  name: "dev3" ,
+  status: "Pending",
+  version: "1.5"
+};
+
+
 ReactDOM.render(
-  <ClusterToggleView mode="thumbnail"></ClusterToggleView>,
+  <div>
+    <ClusterToggleView mode="thumbnail" environment={mockDataThumb1}></ClusterToggleView>
+    <ClusterToggleView mode="thumbnail" environment={mockDataThumb2}></ClusterToggleView>
+    <ClusterToggleView mode="thumbnail" environment={mockDataThumb3}></ClusterToggleView>
+  </div>,
   document.getElementById('container')
 );
 
