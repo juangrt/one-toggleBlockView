814550cc3882749b58d9310c4990f24c76bb2ee4
Juan Garzon <jgarzon@grassroots-tech.com>
2016-03-02 18:00:09 -0500

    Updated bundle files.

--- dist/bundle.js
+++ dist/bundle.js
@@ -60,9 +60,45 @@
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
-	console.log(_ClusterToggleView2.default.THUMBNAIL_MODE);
+	/*
+	 * Accepted Model
+	 * Number - Id (Should be unique)
+	 * Array - Server List
+	 * String - Environment Name
+	 * String - Status
+	 * String?- Version
+	 */
+	var mockDataThumb1 = {
+	  id: 1,
+	  serverList: ["192.168.1.1", "192.168.1.2"],
+	  name: "dev",
+	  status: "Pending",
+	  version: "1.5"
+	};
 	
-	_reactDom2.default.render(_react2.default.createElement(_ClusterToggleView2.default, { mode: 'thumbnail' }), document.getElementById('container'));
+	var mockDataThumb2 = {
+	  id: 1,
+	  serverList: ["192.168.1.1", "192.168.1.2"],
+	  name: "dev2",
+	  status: "Pending",
+	  version: "1.5"
+	};
+	
+	var mockDataThumb3 = {
+	  id: 1,
+	  serverList: ["192.168.1.1", "192.168.1.2"],
+	  name: "dev3",
+	  status: "Pending",
+	  version: "1.5"
+	};
+	
+	_reactDom2.default.render(_react2.default.createElement(
+	  'div',
+	  null,
+	  _react2.default.createElement(_ClusterToggleView2.default, { mode: 'thumbnail', environment: mockDataThumb1 }),
+	  _react2.default.createElement(_ClusterToggleView2.default, { mode: 'thumbnail', environment: mockDataThumb2 }),
+	  _react2.default.createElement(_ClusterToggleView2.default, { mode: 'thumbnail', environment: mockDataThumb3 })
+	), document.getElementById('container'));
 
 /***/ },
 /* 1 */
@@ -19703,7 +19739,7 @@
 	
 	var _ClusterThumbView2 = _interopRequireDefault(_ClusterThumbView);
 	
-	var _ClusterDetailView = __webpack_require__(163);
+	var _ClusterDetailView = __webpack_require__(167);
 	
 	var _ClusterDetailView2 = _interopRequireDefault(_ClusterDetailView);
 	
@@ -19733,6 +19769,8 @@
 	      mode: _this.props.mode
 	    };
 	
+	    console.log(props);
+	
 	    //Create event bindings here
 	    _this.onToggleView = _this.onToggleView.bind(_this);
 	    return _this;
@@ -19748,7 +19786,7 @@
 	    key: 'getView',
 	    value: function getView() {
 	      if (this.state.mode === THUMBNAIL_MODE) {
-	        return _react2.default.createElement(_ClusterThumbView2.default, { data: this.state.mode });
+	        return _react2.default.createElement(_ClusterThumbView2.default, { environment: this.props.environment });
 	      } else {
 	        return _react2.default.createElement(_ClusterDetailView2.default, { data: this.state.mode });
 	      }
@@ -19767,6 +19805,7 @@
 	  return ClusterToggleView;
 	}(_react2.default.Component);
 	
+	ClusterToggleView.propTypes = { mode: _react2.default.PropTypes.string };
 	ClusterToggleView.defaultProps = {
 	  mode: THUMBNAIL_MODE
 	};
@@ -19777,18 +19816,19 @@
 /* 161 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
-
+	"use strict";
+	
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
-
+	
 	var _ClusterThumbView = __webpack_require__(162);
-
+	
 	var _ClusterThumbView2 = _interopRequireDefault(_ClusterThumbView);
-
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
+	
+	__webpack_require__(163);
 	exports.default = _ClusterThumbView2.default;
 
 /***/ },
@@ -19815,31 +19855,78 @@
 	
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 	
+	/*
+	 * Accepted Model
+	 * Number - Id (Should be unique)
+	 * Array - Server List
+	 * String - Environment Name
+	 * String - Status
+	 * String?- Version
+	 */
+	
+	/*
+	{
+	 id: 1,
+	 serverList: ["192.168.1.1" , "192.168.1.2"] ,
+	 name: "dev" ,
+	 status: "Pending",
+	 version: "1.5"
+	}
+	*/
+	
 	var ClusterThumbView = function (_React$Component) {
 	  _inherits(ClusterThumbView, _React$Component);
 	
 	  function ClusterThumbView(props) {
 	    _classCallCheck(this, ClusterThumbView);
 	
+	    //Create event binding here
+	
 	    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(ClusterThumbView).call(this, props));
 	
-	    var _data = [{ ip: "192.168.1.1" }, { ip: "192.168.1.2" }, { ip: "192.168.1.3" }];
-	    _this.state = { data: _data };
-	    //Create event binding here
+	    _this.copyAllClicked = _this.copyAllClicked.bind(_this);
 	    return _this;
 	  }
 	
 	  _createClass(ClusterThumbView, [{
+	    key: "copyAllClicked",
+	    value: function copyAllClicked(event) {
+	      alert("Copy All clicked for: ");
+	    }
+	  }, {
 	    key: "render",
 	    value: function render() {
 	      return _react2.default.createElement(
 	        "div",
-	        null,
+	        { className: "env-block-thumb , env-block-thumb-pending" },
 	        _react2.default.createElement(
 	          "h3",
-	          null,
-	          "(Thumbnail) - Header 3 ",
-	          this.props.data
+	          { id: "title", className: "env-block-thumb-title" },
+	          this.props.environment.name
+	        ),
+	        _react2.default.createElement("div", { className: "env-block-thumb-left" }),
+	        _react2.default.createElement(
+	          "div",
+	          { className: "env-block-thumb-right" },
+	          _react2.default.createElement(
+	            "div",
+	            { onClick: this.copyAllClicked },
+	            "Copy All"
+	          )
+	        ),
+	        _react2.default.createElement(
+	          "div",
+	          { className: "env-block-thumb-footer" },
+	          _react2.default.createElement(
+	            "div",
+	            { style: { float: 'left' } },
+	            this.props.environment.version
+	          ),
+	          _react2.default.createElement(
+	            "div",
+	            { style: { float: 'right', textAlign: 'right' } },
+	            this.props.environment.status
+	          )
 	        )
 	      );
 	    }
@@ -19848,28 +19935,381 @@
 	  return ClusterThumbView;
 	}(_react2.default.Component);
 	
+	//ClusterThumbView.propTypes = { mode: React.PropTypes.string };
+	
 	exports.default = ClusterThumbView;
 
 /***/ },
 /* 163 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
-
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-
-	var _ClusterDetailView = __webpack_require__(164);
-
-	var _ClusterDetailView2 = _interopRequireDefault(_ClusterDetailView);
-
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-	exports.default = _ClusterDetailView2.default;
+	// style-loader: Adds some css to the DOM by adding a <style> tag
+	
+	// load the styles
+	var content = __webpack_require__(164);
+	if(typeof content === 'string') content = [[module.id, content, '']];
+	// add the styles to the DOM
+	var update = __webpack_require__(166)(content, {});
+	if(content.locals) module.exports = content.locals;
+	// Hot Module Replacement
+	if(false) {
+		// When the styles change, update the <style> tags
+		if(!content.locals) {
+			module.hot.accept("!!./../../node_modules/css-loader/index.js!./style.css", function() {
+				var newContent = require("!!./../../node_modules/css-loader/index.js!./style.css");
+				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+				update(newContent);
+			});
+		}
+		// When the module is disposed, remove the <style> tags
+		module.hot.dispose(function() { update(); });
+	}
 
 /***/ },
 /* 164 */
+/***/ function(module, exports, __webpack_require__) {
+
+	exports = module.exports = __webpack_require__(165)();
+	// imports
+	
+	
+	// module
+	exports.push([module.id, ".env-block-thumb {\n  width: 10em;\n  min-height: 10em;\n  float: left;\n  margin: .5em;\n}\n\n.env-block-thumb-left , .env-block-thumb-right{\n  width: 50%\n}\n\n.env-block-thumb-left {\n  float: left;\n  min-height: 5em;\n}\n.env-block-thumb-right {\n  float: right;\n}\n\n.env-block-thumb-footer {\n  clear: both;\n}\n\nh3.env-block-thumb-title {\n  color: #ff0000;\n  text-align: center;\n}\n\n.env-block-thumb-pending {\n  background-color: #ff0;\n}\n\n.env-block-thumb-success {\n  background-color: #0f0;\n}\n\n.env-block-thumb-failure {\n  background-color: #f00;\n}", ""]);
+	
+	// exports
+
+
+/***/ },
+/* 165 */
+/***/ function(module, exports) {
+
+	/*
+		MIT License http://www.opensource.org/licenses/mit-license.php
+		Author Tobias Koppers @sokra
+	*/
+	// css base code, injected by the css-loader
+	module.exports = function() {
+		var list = [];
+	
+		// return the list of modules as css string
+		list.toString = function toString() {
+			var result = [];
+			for(var i = 0; i < this.length; i++) {
+				var item = this[i];
+				if(item[2]) {
+					result.push("@media " + item[2] + "{" + item[1] + "}");
+				} else {
+					result.push(item[1]);
+				}
+			}
+			return result.join("");
+		};
+	
+		// import a list of modules into the list
+		list.i = function(modules, mediaQuery) {
+			if(typeof modules === "string")
+				modules = [[null, modules, ""]];
+			var alreadyImportedModules = {};
+			for(var i = 0; i < this.length; i++) {
+				var id = this[i][0];
+				if(typeof id === "number")
+					alreadyImportedModules[id] = true;
+			}
+			for(i = 0; i < modules.length; i++) {
+				var item = modules[i];
+				// skip already imported module
+				// this implementation is not 100% perfect for weird media query combinations
+				//  when a module is imported multiple times with different media queries.
+				//  I hope this will never occur (Hey this way we have smaller bundles)
+				if(typeof item[0] !== "number" || !alreadyImportedModules[item[0]]) {
+					if(mediaQuery && !item[2]) {
+						item[2] = mediaQuery;
+					} else if(mediaQuery) {
+						item[2] = "(" + item[2] + ") and (" + mediaQuery + ")";
+					}
+					list.push(item);
+				}
+			}
+		};
+		return list;
+	};
+
+
+/***/ },
+/* 166 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/*
+		MIT License http://www.opensource.org/licenses/mit-license.php
+		Author Tobias Koppers @sokra
+	*/
+	var stylesInDom = {},
+		memoize = function(fn) {
+			var memo;
+			return function () {
+				if (typeof memo === "undefined") memo = fn.apply(this, arguments);
+				return memo;
+			};
+		},
+		isOldIE = memoize(function() {
+			return /msie [6-9]\b/.test(window.navigator.userAgent.toLowerCase());
+		}),
+		getHeadElement = memoize(function () {
+			return document.head || document.getElementsByTagName("head")[0];
+		}),
+		singletonElement = null,
+		singletonCounter = 0,
+		styleElementsInsertedAtTop = [];
+	
+	module.exports = function(list, options) {
+		if(false) {
+			if(typeof document !== "object") throw new Error("The style-loader cannot be used in a non-browser environment");
+		}
+	
+		options = options || {};
+		// Force single-tag solution on IE6-9, which has a hard limit on the # of <style>
+		// tags it will allow on a page
+		if (typeof options.singleton === "undefined") options.singleton = isOldIE();
+	
+		// By default, add <style> tags to the bottom of <head>.
+		if (typeof options.insertAt === "undefined") options.insertAt = "bottom";
+	
+		var styles = listToStyles(list);
+		addStylesToDom(styles, options);
+	
+		return function update(newList) {
+			var mayRemove = [];
+			for(var i = 0; i < styles.length; i++) {
+				var item = styles[i];
+				var domStyle = stylesInDom[item.id];
+				domStyle.refs--;
+				mayRemove.push(domStyle);
+			}
+			if(newList) {
+				var newStyles = listToStyles(newList);
+				addStylesToDom(newStyles, options);
+			}
+			for(var i = 0; i < mayRemove.length; i++) {
+				var domStyle = mayRemove[i];
+				if(domStyle.refs === 0) {
+					for(var j = 0; j < domStyle.parts.length; j++)
+						domStyle.parts[j]();
+					delete stylesInDom[domStyle.id];
+				}
+			}
+		};
+	}
+	
+	function addStylesToDom(styles, options) {
+		for(var i = 0; i < styles.length; i++) {
+			var item = styles[i];
+			var domStyle = stylesInDom[item.id];
+			if(domStyle) {
+				domStyle.refs++;
+				for(var j = 0; j < domStyle.parts.length; j++) {
+					domStyle.parts[j](item.parts[j]);
+				}
+				for(; j < item.parts.length; j++) {
+					domStyle.parts.push(addStyle(item.parts[j], options));
+				}
+			} else {
+				var parts = [];
+				for(var j = 0; j < item.parts.length; j++) {
+					parts.push(addStyle(item.parts[j], options));
+				}
+				stylesInDom[item.id] = {id: item.id, refs: 1, parts: parts};
+			}
+		}
+	}
+	
+	function listToStyles(list) {
+		var styles = [];
+		var newStyles = {};
+		for(var i = 0; i < list.length; i++) {
+			var item = list[i];
+			var id = item[0];
+			var css = item[1];
+			var media = item[2];
+			var sourceMap = item[3];
+			var part = {css: css, media: media, sourceMap: sourceMap};
+			if(!newStyles[id])
+				styles.push(newStyles[id] = {id: id, parts: [part]});
+			else
+				newStyles[id].parts.push(part);
+		}
+		return styles;
+	}
+	
+	function insertStyleElement(options, styleElement) {
+		var head = getHeadElement();
+		var lastStyleElementInsertedAtTop = styleElementsInsertedAtTop[styleElementsInsertedAtTop.length - 1];
+		if (options.insertAt === "top") {
+			if(!lastStyleElementInsertedAtTop) {
+				head.insertBefore(styleElement, head.firstChild);
+			} else if(lastStyleElementInsertedAtTop.nextSibling) {
+				head.insertBefore(styleElement, lastStyleElementInsertedAtTop.nextSibling);
+			} else {
+				head.appendChild(styleElement);
+			}
+			styleElementsInsertedAtTop.push(styleElement);
+		} else if (options.insertAt === "bottom") {
+			head.appendChild(styleElement);
+		} else {
+			throw new Error("Invalid value for parameter 'insertAt'. Must be 'top' or 'bottom'.");
+		}
+	}
+	
+	function removeStyleElement(styleElement) {
+		styleElement.parentNode.removeChild(styleElement);
+		var idx = styleElementsInsertedAtTop.indexOf(styleElement);
+		if(idx >= 0) {
+			styleElementsInsertedAtTop.splice(idx, 1);
+		}
+	}
+	
+	function createStyleElement(options) {
+		var styleElement = document.createElement("style");
+		styleElement.type = "text/css";
+		insertStyleElement(options, styleElement);
+		return styleElement;
+	}
+	
+	function createLinkElement(options) {
+		var linkElement = document.createElement("link");
+		linkElement.rel = "stylesheet";
+		insertStyleElement(options, linkElement);
+		return linkElement;
+	}
+	
+	function addStyle(obj, options) {
+		var styleElement, update, remove;
+	
+		if (options.singleton) {
+			var styleIndex = singletonCounter++;
+			styleElement = singletonElement || (singletonElement = createStyleElement(options));
+			update = applyToSingletonTag.bind(null, styleElement, styleIndex, false);
+			remove = applyToSingletonTag.bind(null, styleElement, styleIndex, true);
+		} else if(obj.sourceMap &&
+			typeof URL === "function" &&
+			typeof URL.createObjectURL === "function" &&
+			typeof URL.revokeObjectURL === "function" &&
+			typeof Blob === "function" &&
+			typeof btoa === "function") {
+			styleElement = createLinkElement(options);
+			update = updateLink.bind(null, styleElement);
+			remove = function() {
+				removeStyleElement(styleElement);
+				if(styleElement.href)
+					URL.revokeObjectURL(styleElement.href);
+			};
+		} else {
+			styleElement = createStyleElement(options);
+			update = applyToTag.bind(null, styleElement);
+			remove = function() {
+				removeStyleElement(styleElement);
+			};
+		}
+	
+		update(obj);
+	
+		return function updateStyle(newObj) {
+			if(newObj) {
+				if(newObj.css === obj.css && newObj.media === obj.media && newObj.sourceMap === obj.sourceMap)
+					return;
+				update(obj = newObj);
+			} else {
+				remove();
+			}
+		};
+	}
+	
+	var replaceText = (function () {
+		var textStore = [];
+	
+		return function (index, replacement) {
+			textStore[index] = replacement;
+			return textStore.filter(Boolean).join('\n');
+		};
+	})();
+	
+	function applyToSingletonTag(styleElement, index, remove, obj) {
+		var css = remove ? "" : obj.css;
+	
+		if (styleElement.styleSheet) {
+			styleElement.styleSheet.cssText = replaceText(index, css);
+		} else {
+			var cssNode = document.createTextNode(css);
+			var childNodes = styleElement.childNodes;
+			if (childNodes[index]) styleElement.removeChild(childNodes[index]);
+			if (childNodes.length) {
+				styleElement.insertBefore(cssNode, childNodes[index]);
+			} else {
+				styleElement.appendChild(cssNode);
+			}
+		}
+	}
+	
+	function applyToTag(styleElement, obj) {
+		var css = obj.css;
+		var media = obj.media;
+		var sourceMap = obj.sourceMap;
+	
+		if(media) {
+			styleElement.setAttribute("media", media)
+		}
+	
+		if(styleElement.styleSheet) {
+			styleElement.styleSheet.cssText = css;
+		} else {
+			while(styleElement.firstChild) {
+				styleElement.removeChild(styleElement.firstChild);
+			}
+			styleElement.appendChild(document.createTextNode(css));
+		}
+	}
+	
+	function updateLink(linkElement, obj) {
+		var css = obj.css;
+		var media = obj.media;
+		var sourceMap = obj.sourceMap;
+	
+		if(sourceMap) {
+			// http://stackoverflow.com/a/26603875
+			css += "\n/*# sourceMappingURL=data:application/json;base64," + btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap)))) + " */";
+		}
+	
+		var blob = new Blob([css], { type: "text/css" });
+	
+		var oldSrc = linkElement.href;
+	
+		linkElement.href = URL.createObjectURL(blob);
+	
+		if(oldSrc)
+			URL.revokeObjectURL(oldSrc);
+	}
+
+
+/***/ },
+/* 167 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _ClusterDetailView = __webpack_require__(168);
+	
+	var _ClusterDetailView2 = _interopRequireDefault(_ClusterDetailView);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	__webpack_require__(169);
+	exports.default = _ClusterDetailView2.default;
+
+/***/ },
+/* 168 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -19922,6 +20362,46 @@
 	}(_react2.default.Component);
 	
 	exports.default = ClusterDetailView;
+
+/***/ },
+/* 169 */
+/***/ function(module, exports, __webpack_require__) {
+
+	// style-loader: Adds some css to the DOM by adding a <style> tag
+	
+	// load the styles
+	var content = __webpack_require__(170);
+	if(typeof content === 'string') content = [[module.id, content, '']];
+	// add the styles to the DOM
+	var update = __webpack_require__(166)(content, {});
+	if(content.locals) module.exports = content.locals;
+	// Hot Module Replacement
+	if(false) {
+		// When the styles change, update the <style> tags
+		if(!content.locals) {
+			module.hot.accept("!!./../../node_modules/css-loader/index.js!./style.css", function() {
+				var newContent = require("!!./../../node_modules/css-loader/index.js!./style.css");
+				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+				update(newContent);
+			});
+		}
+		// When the module is disposed, remove the <style> tags
+		module.hot.dispose(function() { update(); });
+	}
+
+/***/ },
+/* 170 */
+/***/ function(module, exports, __webpack_require__) {
+
+	exports = module.exports = __webpack_require__(165)();
+	// imports
+	
+	
+	// module
+	exports.push([module.id, "", ""]);
+	
+	// exports
+
 
 /***/ }
 /******/ ]);
